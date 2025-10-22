b845ab766017127b4d8ae0458b8c35453c75d011
LearningnRunning <rocki.k1208@gmail.com>
2025-10-22 13:21:13 +0900

    fix: cleaned up import statements

--- fastapi/app/db/queries/__init__.py
+++ fastapi/app/db/queries/__init__.py
@@ -4,23 +4,9 @@ SQL 쿼리 모듈들
 
 from . import basic_queries as basic_queries
 from . import item_kakao_mappings_queries as item_kakao_mappings_queries
-)
-from . import (
-    item_kakao_mappings_queries as item_kakao_mappings_queries,
-)
-from . import (
-    items_queries as items_queries,
-)
-from . import (
-    kakao_queries as kakao_queries,
-)
-from . import (
-    kakao_reviewers_queries as kakao_reviewers_queries,
-)
-from . import (
-    kakao_reviews_queries as kakao_reviews_queries,
-)
-from . import (
-    reviews_queries as reviews_queries,
-)
-from . import (
+from . import items_queries as items_queries
+from . import kakao_queries as kakao_queries
+from . import kakao_reviewers_queries as kakao_reviewers_queries
+from . import kakao_reviews_queries as kakao_reviews_queries
+from . import reviews_queries as reviews_queries
+from . import users_queries as users_queries
