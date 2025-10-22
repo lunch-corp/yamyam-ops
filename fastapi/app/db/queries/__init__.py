855fe32d1578c03903ad5d7063e94ca3b3868913
LearningnRunning <rocki.k1208@gmail.com>
2025-10-22 13:11:40 +0900

    fix: cleaned up import statements

--- fastapi/app/db/queries/__init__.py
+++ fastapi/app/db/queries/__init__.py
@@ -2,13 +2,25 @@
 SQL 쿼리 모듈들
 """
 
-from . import (
-    basic_queries,
-    item_kakao_mappings_queries,
-    items_queries,
-    kakao_queries,
-    kakao_reviewers_queries,
-    kakao_reviews_queries,
-    reviews_queries,
-    users_queries,
+from . import basic_queries as basic_queries
+from . import item_kakao_mappings_queries as item_kakao_mappings_queries
 )
+from . import (
+    item_kakao_mappings_queries as item_kakao_mappings_queries,
+)
+from . import (
+    items_queries as items_queries,
+)
+from . import (
+    kakao_queries as kakao_queries,
+)
+from . import (
+    kakao_reviewers_queries as kakao_reviewers_queries,
+)
+from . import (
+    kakao_reviews_queries as kakao_reviews_queries,
+)
+from . import (
+    reviews_queries as reviews_queries,
+)
+from . import (
--- fastapi/app/models/base.py
+++ fastapi/app/models/base.py
@@ -2,6 +2,14 @@ from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 
 from ..utils.ulid_utils import generate_ulid
+from .item import Item
+from .item_kakao_mapping import ItemKakaoMapping
+from .kakao_diner import KakaoDiner
+from .kakao_review import KakaoReview
+from .kakao_reviewer import KakaoReviewer
+from .preference import EmbeddingMetadata, UserPreference
+from .review import Review
+from .user import User
 
 # 모든 모델의 기본 클래스
 Base = declarative_base()
@@ -19,16 +27,7 @@ class ULIDMixin:
     )
 
 
-from .item import Item
-from .item_kakao_mapping import ItemKakaoMapping
-from .kakao_diner import KakaoDiner
-from .kakao_review import KakaoReview
-from .kakao_reviewer import KakaoReviewer
-from .preference import EmbeddingMetadata, UserPreference
-from .review import Review
-
 # 모든 모델을 import하여 SQLAlchemy가 인식할 수 있도록 함
-from .user import User
 
 __all__ = [
     "Base",
