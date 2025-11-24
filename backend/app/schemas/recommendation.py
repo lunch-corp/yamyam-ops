29063e761ffe3e39a568b11174323d488cf44407
bohyun-shin <bohyun.shin@linecorp.com>
2025-11-24 16:27:29 +0900

    feat: update request, response example value in /rec/user/similar api

--- backend/app/schemas/recommendation.py
+++ backend/app/schemas/recommendation.py
@@ -10,8 +10,29 @@ class UserCFRequest(BaseModel):
         ..., description="List of scores which user gave when initial taste discovery"
     )
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "liked_diner_ids": [2000734287, 12386670, 8431628],
+                    "scores_of_liked_diner_ids": [5, 4, 5],
+                }
+            ]
+        }
+    }
+
 
 class UserCFResponse(BaseModel):
     reviewer_id: int = Field(
         ..., description="Reviewer id in kakao review data matched by user cf"
     )
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "reviewer_id": 3933744720,
+                }
+            ]
+        }
+    }
